b52cde2985e2facb9dee823ba8ba369fa4ce7ca6
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-08-23 03:08:27 -0300

    feat: criar UserController para gerenciar endpoints da entidade User

---
+++ src/main/java/com/devsouza/PersonAPI/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.devsouza.PersonAPI.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.devsouza.PersonAPI.entities.User;
+import com.devsouza.PersonAPI.repositories.UserRepository;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+	@Autowired
+	private UserRepository repository;
+	
+	@GetMapping
+	public List<User> findAll() {
+		return repository.findAll();
+
+	}
+
+}
