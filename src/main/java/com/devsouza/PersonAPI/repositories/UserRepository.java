d777161d7cc1dd8abee2fe18da0354b2a5b0d22f
gustavosouzza <gustaavo.goncaalves@gmail.com>
2025-08-23 03:15:54 -0300

    feat: adicionar endpoint GET /users/{id} para buscar usuário por ID

---
+++ src/main/java/com/devsouza/PersonAPI/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.devsouza.PersonAPI.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsouza.PersonAPI.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+	
+	
+}
